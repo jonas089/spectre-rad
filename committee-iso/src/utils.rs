852072dee39883bb0f3caddaf714df396ec52e20
jonas pauli <jonaspauli089@gmail.com>
2024-11-08 12:24:53 +0100

    purge poseidon

--- committee-iso/src/utils.rs
+++ committee-iso/src/utils.rs
@@ -1,13 +1,8 @@
-use ark_bn254::Fr;
 use itertools::Itertools;
-use light_poseidon::Poseidon;
-use light_poseidon::PoseidonBytesHasher;
 use num_bigint::BigUint;
 use sha2::{Digest, Sha256};
-use std::str::FromStr;
 use std::{env, fs};
 
-use crate::constants::DEFAULT_FIELD_MODULUS;
 use crate::{
     types::{Branch, Leaf, PublicKeyHashes, PublicKeys, Root, ZERO_HASHES},
     CommitteeUpdateArgs,
@@ -121,51 +116,17 @@ pub fn precompile_sha2_commitment(keys: Vec<BigUint>, signs: Vec<u8>) -> [u8; 32
     digest(&input).try_into().unwrap()
 }
 
-pub fn poseidon_commit_pubkeys_compressed(keys: Vec<BigUint>, signs: Vec<u8>) -> [u8; 32] {
-    // maximum chunks with circom config: 13
-    // todo: hash more chunks per round
-    let mut poseidon = Poseidon::<Fr>::new_circom(2).unwrap();
-    let mut input: Vec<Vec<u8>> = vec![
-        (keys.get(0).unwrap() % BigUint::from_str(&DEFAULT_FIELD_MODULUS).unwrap()).to_bytes_be(),
-        vec![],
-    ];
-
-    for chunk in signs {
-        let bits = u8_to_bits(chunk);
-        for bit in bits {
-            let mut padded: Vec<u8> = vec![0; 31];
-            padded.push(bit);
-            input[1] = padded;
-            input[0] = poseidon
-                .hash_bytes_be(&input.iter().map(|array| &array[..]).collect::<Vec<&[u8]>>()[..])
-                .unwrap()
-                .to_vec();
-        }
-    }
-    input.get(0).unwrap().as_slice().try_into().unwrap()
-}
-
-fn u8_to_bits(byte: u8) -> [u8; 8] {
-    let mut bits = [0u8; 8];
-    for i in 0..8 {
-        bits[7 - i] = (byte >> i) & 1;
-    }
-    bits
-}
-
 #[cfg(test)]
 mod tests {
-    use super::{decode_pubkeys_x, load_circuit_args_env, poseidon_commit_pubkeys_compressed};
-    use crate::types::CommitteeUpdateArgs;
-    use ark_bn254::Fr;
-    use light_poseidon::{Poseidon, PoseidonBytesHasher};
+    use super::{decode_pubkeys_x, load_circuit_args_env};
+    use crate::{types::CommitteeUpdateArgs, utils::precompile_sha2_commitment};
 
     #[test]
     fn test_poseidon_commit() {
         let args: CommitteeUpdateArgs = load_circuit_args_env();
         let compressed: (Vec<num_bigint::BigUint>, Vec<u8>) =
             decode_pubkeys_x(args.pubkeys_compressed.clone());
-        let commitment = poseidon_commit_pubkeys_compressed(compressed.0, compressed.1);
+        let commitment = precompile_sha2_commitment(compressed.0, compressed.1);
         println!("Commitment: {:?}", &commitment);
     }
 
@@ -174,14 +135,7 @@ mod tests {
         let args: CommitteeUpdateArgs = load_circuit_args_env();
         let compressed: (Vec<num_bigint::BigUint>, Vec<u8>) =
             decode_pubkeys_x(args.pubkeys_compressed.clone());
-        let commitment = poseidon_commit_pubkeys_compressed(compressed.0, compressed.1);
+        let commitment = precompile_sha2_commitment(compressed.0, compressed.1);
         println!("Commitment: {:?}", &commitment);
-    }
-    #[test]
-    fn poseidon_setup() {
-        // BigUint is 32 Bytes
-        let mut poseidon = Poseidon::<Fr>::new_circom(2).unwrap();
-        let hash = poseidon.hash_bytes_be(&[&[1u8; 32], &[2u8; 32]]).unwrap();
-        println!("{:?}", &hash);
     }
 }
