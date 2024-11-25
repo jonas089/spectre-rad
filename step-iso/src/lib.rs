f15cfc24c85ce6c1dfd1d9c6e657a73e3cece49e
jonas pauli <jonaspauli089@gmail.com>
2024-11-25 13:22:50 +0100

    wip: step circuit

--- step-iso/src/lib.rs
+++ step-iso/src/lib.rs
@@ -34,19 +34,60 @@ pub fn aggregate_pubkey(args: SyncStepArgs) -> G1Affine {
             generator = G1Projective::from(generator).double().into();
         }
     }
+    println!("Aggregate Key: {:?}", &generator);
     generator
 }
 
 #[cfg(test)]
 mod tests {
-    use committee_iso::utils::digest;
+    use committee_iso::utils::{digest, merkleize_keys, verify_merkle_proof};
 
     use crate::{aggregate_pubkey, utils::load_circuit_args_env};
     #[test]
     fn test_aggregate_pubkey_commitment() {
         let args = load_circuit_args_env();
-        let aggregated_pubkey = aggregate_pubkey(args);
+        let aggregated_pubkey = aggregate_pubkey(args.clone());
         let commitment = digest(&aggregated_pubkey.to_compressed().to_vec());
+        let signature_bytes = args.signature_compressed;
         println!("Aggregate commitment: {:?}", &commitment);
+
+        todo!("Verify the signature against the aggregated key!")
+    }
+
+    #[test]
+    fn test_verify_roots() {
+        let args: crate::types::SyncStepArgs = load_circuit_args_env();
+
+        verify_merkle_proof(
+            args.execution_payload_branch.to_vec(),
+            args.execution_payload_root,
+            &args.finalized_header.body_root.to_vec(),
+            9,
+        );
+
+        todo!("Fix merkle verification!");
+
+        let attested_header_root = merkleize_keys(vec![
+            args.attested_header.slot.to_be_bytes().to_vec(),
+            args.attested_header.proposer_index.to_be_bytes().to_vec(),
+            args.attested_header.parent_root.to_vec(),
+            args.attested_header.state_root.to_vec(),
+            args.attested_header.body_root.to_vec(),
+        ]);
+
+        let finalized_header_root = merkleize_keys(vec![
+            args.finalized_header.slot.to_be_bytes().to_vec(),
+            args.finalized_header.proposer_index.to_be_bytes().to_vec(),
+            args.finalized_header.parent_root.to_vec(),
+            args.finalized_header.state_root.to_vec(),
+            args.finalized_header.body_root.to_vec(),
+        ]);
+
+        verify_merkle_proof(
+            args.finality_branch,
+            finalized_header_root,
+            &attested_header_root,
+            105,
+        );
     }
 }
