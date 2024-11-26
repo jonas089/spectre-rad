56f836205f5284aec51670beb180f028bcca102c
jonas pauli <jonaspauli089@gmail.com>
2024-11-26 13:46:16 +0100

    sig vrf is failing

--- iso-step/src/lib.rs
+++ iso-step/src/lib.rs
@@ -33,23 +33,33 @@ pub fn aggregate_pubkey(args: SyncStepArgs) -> G1Affine {
 #[cfg(test)]
 mod tests {
     use crate::{aggregate_pubkey, utils::load_circuit_args_env};
-    use committee_iso::utils::{digest, merkleize_keys, uint64_to_le_256, verify_merkle_proof};
+    use blst::min_pk as bls;
+    use blst::BLST_ERROR;
+    use committee_iso::utils::{
+        add_left_right, digest, merkleize_keys, uint64_to_le_256, verify_merkle_proof,
+    };
+
     #[test]
     fn test_aggregate_pubkey_commitment() {
+        const DST: &[u8] = b"BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_POP_";
         let args = load_circuit_args_env();
         let aggregated_pubkey = aggregate_pubkey(args.clone());
         let commitment = digest(&aggregated_pubkey.to_compressed().to_vec());
-        //let signature_bytes = args.signature_compressed;
+        let signature_bytes = args.signature_compressed;
         println!("Aggregate commitment: {:?}", &commitment);
-        /*let attested_header_root = merkleize_keys(vec![
+        let attested_header_root = merkleize_keys(vec![
             uint64_to_le_256(args.attested_header.slot),
             uint64_to_le_256(args.attested_header.proposer_index as u64),
             args.attested_header.parent_root.to_vec(),
             args.attested_header.state_root.to_vec(),
             args.attested_header.body_root.to_vec(),
-        ]);*/
-
-        todo!("Verify the signature against the aggregated key!")
+        ]);
+        let domain = args.domain.to_vec();
+        let signing_root = digest(&add_left_right(attested_header_root, &domain));
+        let signature = bls::Signature::from_bytes(&signature_bytes).unwrap();
+        let public_key = bls::PublicKey::deserialize(&aggregated_pubkey.to_compressed()).unwrap();
+        let res = signature.verify(true, &signing_root, DST, &[], &public_key, true);
+        assert_eq!(res, BLST_ERROR::BLST_SUCCESS);
     }
 
     #[test]
