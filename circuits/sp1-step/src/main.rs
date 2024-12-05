f8481e9cbcd783cd75dfd25cd0227ca2c7ba3e7a
jonas pauli <jonaspauli089@gmail.com>
2024-12-05 12:12:20 +0100

    add output to sp1 step circuit

--- circuits/sp1-step/src/main.rs
+++ circuits/sp1-step/src/main.rs
@@ -1,11 +1,13 @@
 #![no_main]
 
 use committee_iso::utils::{merkleize_keys, uint64_to_le_256, verify_merkle_proof};
-use step_iso::{types::SyncStepArgs, verify_aggregate_signature};
+use step_iso::{
+    types::{SyncStepArgs, SyncStepCircuitOutput},
+    verify_aggregate_signature,
+};
 sp1_zkvm::entrypoint!(main);
 
 pub fn main() {
-    todo!("SP1 is not yet fully supported, please wait for this warning to be removed!");
     let args: SyncStepArgs = serde_json::from_slice(&sp1_zkvm::io::read_vec()).unwrap();
     verify_merkle_proof(
         args.execution_payload_branch.to_vec(),
@@ -14,7 +16,7 @@ pub fn main() {
         9,
     );
 
-    let finalized_header_root = merkleize_keys(vec![
+    let finalized_header_root: Vec<u8> = merkleize_keys(vec![
         uint64_to_le_256(args.finalized_header.slot),
         uint64_to_le_256(args.finalized_header.proposer_index as u64),
         args.finalized_header.parent_root.to_vec(),
@@ -24,10 +26,15 @@ pub fn main() {
 
     verify_merkle_proof(
         args.finality_branch.clone(),
-        finalized_header_root,
+        finalized_header_root.clone(),
         &args.attested_header.state_root.to_vec(),
         105,
     );
+    let aggregate_key_commitment: Vec<u8> = verify_aggregate_signature(args.clone());
+    let output = SyncStepCircuitOutput {
+        finalized_block_root: finalized_header_root.try_into().unwrap(),
+        aggregate_key_commitment: aggregate_key_commitment.try_into().unwrap(),
+    };
 
-    verify_aggregate_signature(args.clone());
+    // todo: commit output
 }
