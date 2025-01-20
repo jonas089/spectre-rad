ee7f9a20793acb006f7e8ace41cd49bbc745d551
jonas pauli <jonaspauli089@gmail.com>
2025-01-20 12:41:50 +0100

    update tests for benchmark

--- prover/tests/integration.rs
+++ prover/tests/integration.rs
@@ -11,7 +11,61 @@ mod tests {
     use std::process::Command;
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn test_committee_update_beacon_cli_e2e() {
+    async fn test_committee_update_beacon_cli_e2e_groth16() {
+        let prune_out = Command::new("docker")
+            .arg("system")
+            .arg("prune")
+            .arg("-a")
+            .arg("-f")
+            .output()
+            .expect("Failed to prune");
+        println!(
+            "Docker prune out: {}",
+            &String::from_utf8(prune_out.stdout).unwrap()
+        );
+        let path = Path::new("/Users/chef/.sp1/circuits/plonk/v3.0.0");
+        if tokio::fs::metadata(path).await.is_ok() {
+            tokio::fs::remove_dir_all(path)
+                .await
+                .expect("Failed to remove directory");
+        }
+        let ((s, c), oc) = get_light_client_update().await;
+        let (committee_proof, committee_vk) = generate_committee_update_proof_sp1(
+            &prover::ProverOps::Default,
+            c,
+            &prover::ProofCompressionBool::Compressed,
+        );
+        let committee_public_values = committee_proof.public_values.to_vec();
+        let committee_inputs = RecursiveInputs {
+            public_values: committee_public_values,
+            vk: committee_vk.hash_u32(),
+        };
+
+        let pubkeys_x_decoded = decode_pubkeys_x(oc.clone());
+        let commitment = commit_to_keys_with_sign(&pubkeys_x_decoded.0, &pubkeys_x_decoded.1);
+
+        let (step_proof, step_vk) = generate_step_proof_sp1(
+            &prover::ProverOps::Default,
+            commitment,
+            s,
+            &prover::ProofCompressionBool::Compressed,
+        );
+        let step_public_values = step_proof.public_values.to_vec();
+
+        let step_inputs = RecursiveInputs {
+            public_values: step_public_values,
+            vk: step_vk.hash_u32(),
+        };
+        let (_proof, _vk) = generate_aggregate_proof_sp1(
+            &prover::ProverOps::Groth16,
+            vec![committee_inputs, step_inputs],
+            vec![committee_proof, step_proof],
+            vec![committee_vk, step_vk],
+        );
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_committee_update_beacon_cli_e2e_plonk() {
         let prune_out = Command::new("docker")
             .arg("system")
             .arg("prune")
